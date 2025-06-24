54a56b2ee3569a8a9d4d60e7eef078cd23773489
fiebryhoga <dimasfiebry@gmail.com.com>
2025-06-24 22:24:15 +0700

    change layanan page

--- src/pages/layanan.jsx
+++ src/pages/layanan.jsx
@@ -1,5 +1,5 @@
 // pages/layanan.js (File utama halaman Layanan)
-import Layout from "./Layout"; // Pastikan path ke Layout Anda benar
+import Layout from "./layout"; // Pastikan path ke Layout Anda benar
 import HeroLayanan from "../components/layanan/HeroLayanan";
 import DetailLayanan from "../components/layanan/DetailLayanan";
 import ProcessSection from "../components/layanan/ProcessSection";
